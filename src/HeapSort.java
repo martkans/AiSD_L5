384346e44c8692d2d07b00a5223efd5e02cdc31f
martkans <martkans@outlook.com>
2018-03-29 15:57:42 +0200

    Heap sort - done

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -1,5 +1,39 @@
 public class HeapSort {
-    public static int[] heapSort(int T[]){
-        return new int[]{1,2,3};
+
+    public static int[] heapSort(int T[]) {
+        if (T == null) throw new NullPointerException();
+
+        for (int i = (T.length/2) - 1; i >= 0; i--)
+            heap(T, T.length, i);
+
+        int i = T.length - 1;
+        while (i >= 0){
+            int temp = T[0];
+            T[0] = T[i];
+            T[i] = temp;
+            heap(T, i, 0);
+            i--;
+        }
+        return T;
+    }
+
+    private static void heap(int T[], int size, int root){
+        int left = 2 * root + 1;
+        int right = 2 * root + 2;
+        int max = root;
+
+        if(left < size && T[left] > T[max])
+            max = left;
+
+        if (right < size && T[right] > T[max])
+            max = right;
+
+        if (max != root){
+            int temp = T[root];
+            T[root] = T[max];
+            T[max] = temp;
+
+            heap(T, size, max);
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
         int T[] = MergeSort.mergeSort(new int[]{7,9,2,1,8,3});
         for (int i:T)
             System.out.print(i + " ");
+
+        System.out.println("\n");
+        int R[] = HeapSort.heapSort(new int[]{8,1,2,9,10,4});
+        for (int i:R)
+            System.out.print(i + " ");
     }
 }
